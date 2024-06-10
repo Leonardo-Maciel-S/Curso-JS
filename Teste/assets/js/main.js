cc2b21d1ba1431016ce6a28d92e99ed8bf9bfd01
Leonardo Maciel <leonardomacielj@gmail.com>
2024-06-10 17:59:39 -0300

    create projetc timer

--- Teste/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-const paragraphs = document.querySelectorAll('p')
-
-const styleComputed = getComputedStyle(document.querySelector('body'))
-
-for (let i of paragraphs) {
-    i.style.backgroundColor = 
-    console.log(styleComputed.backgroundColor)
-}
---
+++ Timer/assets/js/main.js
@@ -0,0 +1,73 @@
+let time0 = new Date(0)
+let atual = 0
+let clicked = 0
+let interval;
+
+const timer = document.querySelector('.timer')
+const play = document.querySelector('.play');
+const pause = document.querySelector('.pause');
+const clear = document.querySelector('.clear');
+
+function changeTimer() {
+    atual += 1000
+    time0 = new Date(atual)
+    return time0.toLocaleTimeString('pt-BR', {
+        hour12: false, 
+        timeZone: 'UTC'
+    })
+} 
+
+function playTimer() {
+    timer.style.color = 'black'
+
+    interval = setInterval(function () {
+        timer.innerHTML = changeTimer() 
+        console.log(atual)
+    }, 1000);
+}
+
+function pauseTImer() {
+    clearInterval(interval)
+
+    timer.style.color = 'red'
+}
+
+function clearTimer() {
+    clearInterval(interval)  
+    
+    timer.style.color = 'black'
+    atual = 0
+    timer.innerHTML = '00:00:00'
+}
+
+
+
+play.addEventListener('click', () => {
+    if (clicked > 0) {
+        return 
+    }
+
+    playTimer()
+
+    clicked = 1
+})
+
+
+pause.addEventListener('click', () => {
+    pauseTImer()
+    clicked = 0
+})
+
+
+clear.addEventListener('click', () => {
+    clearTimer()
+    clicked = 0
+})
+
+
+
+
+
+
+
+
