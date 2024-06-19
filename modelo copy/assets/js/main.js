d8c2e35df0a87869987264e5a0a9e300609b7b19
Leonardo Maciel <leonardomacielj@gmail.com>
2024-06-19 17:44:19 -0300

    test of code

--- modelo copy/assets/js/main.js
+++ modelo copy/assets/js/main.js
@@ -1,6 +1,28 @@
-const nomes = ['Lucas', 'Maria', 'João', 'Leonardo']
+function Produto(nome, valor, estoque) {
 
-const nomes2 = nomes.splice(-3, nomes.length)
+    let nomePrivado = nome;
 
+    Object.defineProperties(this, {
+        nome: {
+            enumerable: true,   // se vai mostrar a chave num iterável
+            configurable: true,  // reconfigurável ou apagável
+            get: () => {
+                return nomePrivado
+            }, 
+            set: (valor) => {
+                if (valor.toLowerCase() === 'leonardo') {
+                    nomePrivado = 'gostoso'
+                    return
+                }
 
-console.log(nomes, nomes2)
+                nomePrivado = nome
+
+            }
+        },
+    })
+   
+}
+
+const produto1 = new Produto('camiseta', 20, 3)
+produto1.nome = 'Leonardo'
+console.log(produto1.nome)
